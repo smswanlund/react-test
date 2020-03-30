1d7dcab8b074638e235924656afc2534426236af
smswanlund <muelmac@gmail.com>
2020-03-30 08:29:56 -0400

    Updates

---
+++ precache-manifest.240218a7866d2d0aa798be5826b9f98b.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "91e87af5d0bfc2a79f8e",
+    "url": "/react-test/static/js/runtime~main.b9ad4e01.js"
+  },
+  {
+    "revision": "53d64b5963b662aa23e1",
+    "url": "/react-test/static/js/main.dbe7d933.chunk.js"
+  },
+  {
+    "revision": "c20da4998ed8f36ee8dd",
+    "url": "/react-test/static/js/2.26e993ba.chunk.js"
+  },
+  {
+    "revision": "53d64b5963b662aa23e1",
+    "url": "/react-test/static/css/main.06d82cc4.chunk.css"
+  },
+  {
+    "revision": "16172b71c6eb0887cc9c6c7dc2cb9e6c",
+    "url": "/react-test/index.html"
+  }
+];
